17fb6d5449118c800144e3922b3b7cb4bcfd350c
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-01-26 18:44:45 -0600

    creating models

---
+++ Develop/models/foodTag.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
+
+class foodTag extends Model {}
+
+foodTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    food_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: food,
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
