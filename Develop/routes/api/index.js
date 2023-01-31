6ade68acff88e72fa5ebc6ca6982f75645636a9c
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 19:58:03 -0600

    added functionality to index routes

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,1 +1,12 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const foodRoutes = require('./foodRoutes');
+const typeRoutes = require('./typeRoutes');
+const tagRoutes = require('./tagRoutes');
+
+router.use('/users', userRoutes);
+router.use('/food', foodRoutes);
+router.use('/type', typeRoutes);
+router.use('/tag', tagRoutes);
+
+module.exports = router;
