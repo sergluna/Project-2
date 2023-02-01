2456f8080f60f3e0bf9718be0d2d23baff8729f7
rdalby2002 <ryancdalby@gmail.com>
2023-01-31 20:54:55 -0600

    Fleshed out main page, added a login/signup page

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,4 +3,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.get('/', (req, res) => {
+  res.render('food')
+});
+
+router.get('/login', (req, res) => {
+  res.render('login')
+});
+
 module.exports = router;
